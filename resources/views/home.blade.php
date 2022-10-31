d2fee557474aa4602936a6fb6c692d8049615705
Rizki Pratama <rizki.pezzek@gmail.com>
2022-10-31 17:53:53 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,141 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+<div class="container">
+    <h2>Skripsi dan KP Mahasiswa</h2>
+    <div class="row">
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Form Pengajuan Proposal Skripsi & KP</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://forms.gle/EDbHR9zuNvuUaojr7" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Form Pendaftaran Sidang Kerja Praktek</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://forms.gle/wmhEVRm5iJHb9Q1z5" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Form Pendaftaran Sidang Skripsi</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://forms.gle/EDbHR9zuNvuUaojr7" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Form Pendaftaran Sertifikasi SKPI</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://s.id/sertifikasistmikbdg" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Panduan Kerja Praktek</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Dokumen</h6>
+                    <a href="https://s.id/12nl0" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-download"></i> Download
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Panduan Skripsi</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Dokumen</h6>
+                    <a href="https://s.id/12nkC" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-download"></i> Download
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Permintaan Form Bimbingan</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://s.id/12orz" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Data Skripsi dan Sertifikasi SKPI</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://forms.gle/TBzC8mfEypq39wJY8" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h2>Mini Project</h2>
+    <div class="row">
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Form Pengumpulan Mini Project</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSccsE5sscgR3M7seS8qrth3TMJTzhKlc2b-5Nd8Leq7cXSj9Q/viewform" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h2>Surat Edaran Akademik</h2>
+    <div class="row">
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Surat Edaran Akademik</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Dokumen</h6>
+                    <a href="https://s.id/12QGm" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-download"></i> Download
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h2>Form Ujian</h2>
+    <div class="row">
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Form Ujian Susulan</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://forms.gle/ATeBdjMWD6ivmstx9" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container py-3">
+        <div class="d-flex">
+            <span>&copy; 2022 STMIK Bandung</span>
+        </div>
+    </div>
+</footer>
