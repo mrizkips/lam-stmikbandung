b938c473fa9409d05f2d8b4bc48870d8c83efdd0
Rizki Pratama <rizki.pezzek@gmail.com>
2022-10-31 18:15:57 +0700

    Home finished

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,6 @@
 
 @section('content')
 <div class="container">
-    <h2>Skripsi dan KP Mahasiswa</h2>
     <div class="row">
         <div class="col-sm-6 col-lg-4 mb-3">
             <div class="card">
@@ -94,9 +93,6 @@
                 </div>
             </div>
         </div>
-    </div>
-    <h2>Mini Project</h2>
-    <div class="row">
         <div class="col-sm-6 col-lg-4 mb-3">
             <div class="card">
                 <div class="card-body">
@@ -108,9 +104,6 @@
                 </div>
             </div>
         </div>
-    </div>
-    <h2>Surat Edaran Akademik</h2>
-    <div class="row">
         <div class="col-sm-6 col-lg-4 mb-3">
             <div class="card">
                 <div class="card-body">
@@ -122,15 +115,45 @@
                 </div>
             </div>
         </div>
-    </div>
-    <h2>Form Ujian</h2>
-    <div class="row">
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Permohonan Surat Kelakuan Baik</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://s.id/12QGm" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
         <div class="col-sm-6 col-lg-4 mb-3">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Form Ujian Susulan</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
                     <a href="https://forms.gle/ATeBdjMWD6ivmstx9" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Kuisioner Tracer Study</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://forms.gle/VJqRRkQjBKcGEXAs7" class="btn btn-primary" target="_link">
+                        <i class="fa-solid fa-link"></i> Buka Link
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Angket Evaluasi Pembelajaran</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Google Form</h6>
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLScYGXvpSTyUtxQppPDbVcU6kNQ1tnp-O7Z1HQ8zqLWmmpOUmA/viewform" class="btn btn-primary" target="_link">
                         <i class="fa-solid fa-link"></i> Buka Link
                     </a>
                 </div>
