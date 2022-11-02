8d1279ca37afe6ff9d8760041a23e264075e356f
Rizki Pratama <rizki.pezzek@gmail.com>
2022-11-02 14:52:25 +0700

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title') - {{ config('app.name') }}</title>
+        <link rel="shortcut icon" href="{{ Vite::asset('resources/images/favicon.ico') }}" type="image/x-icon">
         @vite(['resources/js/app.js'])
     </head>
     <body>
